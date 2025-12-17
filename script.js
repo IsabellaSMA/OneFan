2405bbdff98da151c2e39b5dace4f7b73f2c30f7
IsabellaSMA <silvaisabella721@gmail.com>
2025-12-17 01:39:45 -0300

    :bookmark: Estrutura do projeto criada e organização de arquivos/pastas

---
+++ script.js
@@ -0,0 +1,15 @@
+function abrirTwoFans() {
+    // Abre 'nova_tela.html' em uma nova aba (_blank)
+    window.open("twofans.html", "_blank");
+  }
+
+function abrirThreeFans() {
+    // Abre 'nova_tela.html' em uma nova aba (_blank)
+    window.open("threefans.html", "_blank");
+  }
+
+function abrirFourFans() {
+    // Abre 'nova_tela.html' em uma nova aba (_blank)
+    window.open("fourfans.html", "_blank");
+  }
+
